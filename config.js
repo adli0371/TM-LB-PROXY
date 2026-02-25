bad8f13baaebee8adbfe5f245004e8642b86e5f1
Adli Hussain <adli.hussain@live.com>
2026-02-25 19:37:21 +0700

    Move hardcoded URLs into separate config.js file
    Uploaded HTML's as well so that vercel can deploy them directly.

---
+++ config.js
@@ -0,0 +1,17 @@
+/* =========================
+   EDIT ONLY THIS FILE
+========================= */
+
+const CONFIG = {
+  PROXY_BASE: "https://tm-lb-proxy.vercel.app/api/proxy?path=",
+
+  QUALI: {
+    COMP_ID: 41289,
+    CHALLENGE_ID: 19445
+  },
+
+  WR: {
+    CAMPAIGN_UID: "NLS-3bSJJT2tUkmz2kZPUcVEfGPe2E35kcm4kop",
+    MAP_UID: "BtYymvmqtzeD6rr8YU5PIwyH6oa"
+  }
+};
