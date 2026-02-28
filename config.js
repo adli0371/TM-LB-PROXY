f2f2992d3149296dbc69120614d50d555f57f874
Adli Hussain <adli.hussain@live.com>
2026-02-28 17:06:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,26 @@
 const CONFIG = {
   PROXY_BASE: "https://tm-lb-proxy.vercel.app/api/proxy?path=",
 
+  // NEW STANDINGS SECTION
+  STANDINGS: {
+    MATCHES: [
+      "comp/41163/match/132456/0",
+      "comp/41289/match/132863/0"
+    ],
+    TEAMS: [
+      { keywords: ["Bren", "TwistedMinds"], fullName: "Twisted Minds" },
+      { keywords: ["Gwen", "Binkss", "AGG"], fullName: "All Gamers Global" },
+      { keywords: ["Tjalic", "Glast", "BS+"], fullName: "BS+COMPETITION" },
+      { keywords: ["CarlJr", "Spam", "FAL"], fullName: "Falcons Esports" },
+      { keywords: ["Massa", "Granady", "BIG"], fullName: "BIG" },
+      { keywords: ["Epos", "Liquid", "TL"], fullName: "Team Liquid" },
+      { keywords: ["Elconn", "NEM"], fullName: "Nemesis" },
+      { keywords: ["Mime", "NAVI"], fullName: "Natus Vincere" },
+      { keywords: ["Wosile", "GEN"], fullName: "Gen.G" },
+      { keywords: ["Nayko", "FUT"], fullName: "FUT Esports" }
+    ]
+  },
+
   QUALI: {
     COMP_ID: 41289,
     CHALLENGE_ID: 19445
